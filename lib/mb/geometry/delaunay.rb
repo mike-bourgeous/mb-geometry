98feab881ac17a3132a5a1102ea286eb82f10267
Mike Bourgeous <mike@mikebourgeous.com>
2021-06-03 13:36:25 -0600

    Added more documentation to MB::Geometry::Delaunay.

--- lib/mb/geometry/delaunay.rb
+++ lib/mb/geometry/delaunay.rb
@@ -1,3 +1,5 @@
+# Switches between fast version and debuggable/visualizable version of the
+# pure-Ruby Delaunay triangulation algorithm.
 if ENV['DELAUNAY_DEBUG'] || (defined?($delaunay_debug) && $delaunay_debug)
   $delaunay_debug = true
   require_relative 'delaunay_debug'
